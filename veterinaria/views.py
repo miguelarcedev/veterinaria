6e7f817c8e0ed36bc28c24f6307c2e67e7b0c3cc
Miguel Arce <correo.arce@gmail.com>
2023-04-29 17:00:59 -0300

    prueba

---
+++ veterinaria/views.py
@@ -0,0 +1,14 @@
+
+from django.shortcuts import render
+
+def index(request):
+   
+    return render(request, "index.html")
+
+def historia(request):
+   
+    return render(request, "historia.html")
+
+def institucional(request):
+   
+    return render(request, "institucional.html")
