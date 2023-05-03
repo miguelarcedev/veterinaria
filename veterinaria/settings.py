766e5d14318cfc339c98810aa4e49c8c49cbfac0
miguelarcedev <correo.arce@gmail.com>
2023-05-03 11:32:25 -0300

    media

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
